ca795d53110e2de37fdbae8fe9a6ae5c54ab9691
javason <javason@hanmail.net>
2020-06-28 22:16:54 +0900

    Updates

---
+++ precache-manifest.9c7fa5d85c2f4515d62c9bd843ada22d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbbbbc048e1781beae71b7f083e41faa",
+    "url": "/javason/movie_app_2020/index.html"
+  },
+  {
+    "revision": "2a53cb75b4c43cbe38ba",
+    "url": "/javason/movie_app_2020/static/css/main.8b996233.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/javason/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/javason/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a53cb75b4c43cbe38ba",
+    "url": "/javason/movie_app_2020/static/js/main.faa8e758.chunk.js"
+  },
+  {
+    "revision": "96b30ccbe16f40e812b1",
+    "url": "/javason/movie_app_2020/static/js/runtime-main.e5cfcebd.js"
+  }
+]);
